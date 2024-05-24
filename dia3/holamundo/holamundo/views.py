350b76c8e805ec432325e901ad3374e9a482259b
J.aguilera <j.aguilera.duhalde@gmail.com>
2024-05-23 22:58:48 -0400

    desafiodjango

--- dia3/holamundo/holamundo/views.py
+++ dia3/holamundo/holamundo/views.py
@@ -4,17 +4,17 @@ from django.http import HttpResponse
 
 # Create your views here.
 def hola(request):
-    return HttpResponse("""
-    <h1>Hola Mundo!!!</h1>
-    <p>Esta es mi primera aplicación con Django mostrando HTML</p>
-    """)
+        return HttpResponse("""
+                <h1>Hola Mundo!!!</h1>
+                <p>Esta es mi primera aplicación con Django mostrando HTML</p>
+        """)
 
 
 def test(request):
         return HttpResponse("""
-            <h1>Esta es otra pagina</h1>
-            <p>este es otro parrafo</p>
-            <img src="" alt="imagen de prueba"
+                <h1>Esta es otra pagina</h1>
+                <p>este es otro parrafo</p>
+                <img src="" alt="imagen de prueba"
         """)
         
 
@@ -39,47 +39,46 @@ def home(request):
                 </body>
 
                 </html>                   
-                            
         """)
         
         
 
 def about(request):
         return render(request,"about.html")
-    
-    
-    
+
+
+
 def contact(request):
         return HttpResponse("""
         <!doctype html>
-<html lang="en">
+        <html lang="en">
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" 
+        <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Bootstrap demo</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" 
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-</head>
+        </head>
 
-<body>
-    <div class="container">
-        <div class="mb-3">
-            <label for="exampleFormControlInput1" class="form-label">Email address</label>
-            <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+        <body>
+        <div class="container">
+                <div class="mb-3">
+                        <label for="exampleFormControlInput1" class="form-label">Email address</label>
+                        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
         </div>
         <div class="mb-3">
-            <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
-            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
+                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
         </div>
 
-    </div>
+        </div>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" 
-    integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" 
-    crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" 
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" 
+        crossorigin="anonymous"></script>
 </body>
 
 </html>                   
-                            
+
         """)
---
+++ dia4/modos.py
@@ -0,0 +1,17 @@
+# python -m venv oflans
+
+# source oflans/Scripts/activate
+
+# pip install django
+
+# pip list
+
+# django-admin startproject onliFlans
+
+# cd onliFlans
+
+# python manage.py runserver
+
+# code . 
+
+# C:\python\M6_DJANGO\dia4\onliFlans\onliFlans\assets\img
